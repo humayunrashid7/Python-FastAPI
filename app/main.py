1d608536644f5442ddbc5ca25149b65df6a949e5
Humayun Rashid <humayunrashid@outlook.com>
2022-02-28 16:49:11 -0500

    added CORS support

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.app_v1.routers import posts
 
+# Declare App
 app = FastAPI()
 
+# Config CORS
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+#Config Routing
 app.include_router(posts.router)
 
+# API Root Method
 @app.get('/')
 async def root():
     return {'message': 'Hello World!'}
